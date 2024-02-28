06ce90043b15ebf3f4912a40cb828818a1ab9569
Audrey Luquet <audrey.luquet@tutanota.com>
2024-02-28 11:17:53 +0100

    ajout Header + Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Projects from "./pages/Projects"
@@ -11,7 +13,7 @@ import "./App.css"
 export default function App() {
    return (
       <div>
-         <p>BLA BLA BLA !!!</p>
+         <Header />
 
          <Router>
             <Routes>
@@ -22,6 +24,8 @@ export default function App() {
                <Route path="/Contact" element={<Contact />} />
             </Routes>
          </Router>
+
+         <Footer />
       </div>
    )
 }
