cfbe60945526cdb62b365d8de15b7a0070f8fa8b
Audrey Luquet <audrey.luquet@tutanota.com>
2024-02-28 14:31:27 +0100

    dbt header (erreur étrange avec useContext bien que pas utilisé)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 import Header from "./components/Header"
 import Footer from "./components/Footer"
@@ -7,23 +7,43 @@ import About from "./pages/About"
 import Projects from "./pages/Projects"
 import Techno from "./pages/Techno"
 import Contact from "./pages/Contact"
+import Error from "./pages/Error"
 
 import "./App.css"
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <Home />,
+   },
+   {
+      path: "/About",
+      element: <About />,
+   },
+   {
+      path: "/Projects",
+      element: <Projects />,
+   },
+   {
+      path: "/Techno",
+      element: <Techno />,
+   },
+   {
+      path: "/Contact",
+      element: <Contact />,
+   },
+   {
+      path: "/*",
+      element: <Error />,
+   },
+])
 
 export default function App() {
    return (
       <div>
          <Header />
 
-         <Router>
-            <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="/About" element={<About />} />
-               <Route path="/Projects" element={<Projects />} />
-               <Route path="/Techno" element={<Techno />} />
-               <Route path="/Contact" element={<Contact />} />
-            </Routes>
-         </Router>
+         <RouterProvider router={router} />
 
          <Footer />
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
+// import { NavLink } from "react-router-dom"
+
 export default function Header() {
    return (
-      <main>
+      <header>
          <p>HEADER !!!</p>
-      </main>
+         <nav>
+            <a href="/">Accueil</a>
+            <a href="/About">À Propos</a>
+            <a href="/Projects">Projets</a>
+            <a href="/Techno">Technologies</a>
+            <a href="/Contact">Contact</a>
+         </nav>
+      </header>
    )
 }
