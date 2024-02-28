caaf93f016518e1d18e1dcbcdfc34bc36995c24d
Audrey Luquet <audrey.luquet@tutanota.com>
2024-02-28 11:14:27 +0100

    mise en place des routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import logo from "./logo.svg"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Techno from "./pages/Techno"
+import Contact from "./pages/Contact"
 
 import "./App.css"
 
 export default function App() {
-   return <div className="App">BLA BLA BLA !!!</div>
+   return (
+      <div>
+         <p>BLA BLA BLA !!!</p>
+
+         <Router>
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/About" element={<About />} />
+               <Route path="/Projects" element={<Projects />} />
+               <Route path="/Techno" element={<Techno />} />
+               <Route path="/Contact" element={<Contact />} />
+            </Routes>
+         </Router>
+      </div>
+   )
 }
