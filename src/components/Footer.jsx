aea84b8904f29bc8b414150440dd7908129df9e5
Audrey Luquet <audrey.luquet@tutanota.com>
2024-02-29 09:25:13 +0100

    ajout style Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
+import styled from "styled-components"
+import { mixins } from "../styles/mixins"
+import { colors, fontSize } from "../styles/variables"
+const MiniFooter = styled.footer`
+   width: 100%;
+   height: 25px;
+   background-color: ${colors.bg_tertiary};
+   ${mixins.totalCenter};
+   font-size: ${fontSize.mini_txt};
+   color: ${colors.txt_primary};
+`
+
 export default function Footer() {
    return (
-      <main>
-         <p>FOOTER !!!</p>
-      </main>
+      <MiniFooter>
+         <p>Portfolio d'Audrey Luquet</p>
+      </MiniFooter>
    )
 }
--- src/styles/variables.jsx
+++ src/styles/variables.jsx
@@ -6,9 +6,12 @@ export const fontFamily = {
 export const colors = {
    txt_primary: "#FFFFFF", // blanc
    bg_primary: "#05092d", // bleu foncé
+   bg_secondary: "#0279c5", // bleu clair (à mettre en transparent)
+   bg_tertiary: "#000000", // noir
 }
 
 export const fontSize = {
-   title: "",
+   title: "22px",
    subtitle: "16px",
+   mini_txt: "11px",
 }
