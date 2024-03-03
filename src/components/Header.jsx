b663f4b0bd5d423c8c0d52fd6d53eeffd8daff8d
Audrey Luquet <audrey.luquet@tutanota.com>
2024-03-03 14:10:24 +0100

    ajout FontAwesome + modif Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,18 @@
-import { NavLink } from "react-router-dom"
-
-import Logo_GitHub from "../assets/github.svg"
-import Logo_LinkedIn from "../assets/linkedin.svg"
+// import { NavLink } from "react-router-dom"
 
 import styled from "styled-components"
 import { mixins } from "../styles/mixins"
 import { colors, fontFamily, fontSize } from "../styles/variables"
 const StickyHeader = styled.header`
    width: 100%;
-   height: 50px;
-   background-color: ${colors.bg_primary};
+   height: 60px;
+   background-color: ${colors.bg_tertiary};
    ${mixins.rowSpaceBetween};
    align-items: center;
    position: sticky;
+   padding: 0 10px 0 10px;
 `
-const SlyledLink = styled(NavLink)`
+/* const SlyledLink = styled(NavLink)`
    padding: 5px;
    font-family: ${fontFamily.ff_secondary};
    font-size: ${fontSize.subtitle};
@@ -22,6 +20,11 @@ const SlyledLink = styled(NavLink)`
    &:hover {
       cursor: pointer;
    }
+` */
+const Icon = styled.i`
+   font-size: 24px;
+   color: ${colors.txt_primary};
+   padding: 0 3px 0 3px;
 `
 
 export default function Header() {
@@ -29,15 +32,16 @@ export default function Header() {
       <StickyHeader>
          <p>HEADER !!!</p>
          <nav>
-            <SlyledLink href="/">Accueil</SlyledLink>
+            blabla
+            {/* <SlyledLink href="/">Accueil</SlyledLink>
             <SlyledLink href="/About">À Propos</SlyledLink>
             <SlyledLink href="/Projects">Projets</SlyledLink>
             <SlyledLink href="/Techno">Technologies</SlyledLink>
-            <SlyledLink href="/Contact">Contact</SlyledLink>
+            <SlyledLink href="/Contact">Contact</SlyledLink> */}
          </nav>
          <nav>
-            <img src={Logo_GitHub} alt="logo de GitHub" />
-            <img src={Logo_LinkedIn} alt="logo de LinkedIn" />
+            <Icon className="fa-brands fa-github" alt="logo de GitHub"></Icon>
+            <Icon className="fa-brands fa-linkedin" alt="logo de LinkedIn"></Icon>
          </nav>
       </StickyHeader>
    )
