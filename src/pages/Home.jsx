395e0ae80e47c9ee8054de008e8c31421990a3cf
Audrey Luquet <audrey.luquet@tutanota.com>
2024-03-03 15:04:19 +0100

    correction mise en page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,57 @@
+import Moi from "../assets/Moi3.jpg"
+
 import styled from "styled-components"
 import { mixins } from "../styles/mixins"
 import { colors, fontFamily, fontSize } from "../styles/variables"
 const Main = styled.main`
    ${mixins.totalCenter};
+   margin: 70px 0;
+   position: relative;
 `
 const Shape = styled.div`
    width: 800px;
-   height: 520px;
+   height: 480px;
+   ${mixins.totalCenter};
    background-color: ${colors.bg_secondary};
-   opacity: 0.5;
-   border-radius: 56% 44% 33% 67%;
+   opacity: 0.6;
+   border-radius: 65% 35% 42% 58% / 67% 40% 60% 33%;
    ${mixins.rowSpaceBetween};
    align-items: center;
-`
-const Img = styled.img`
-   width: 120px;
-   height: 480px;
-   border: solid 2px pink;
+   position: relative;
+   rigth: -50%;
+   top: 10%;
 `
 const Section = styled.section`
-   width: auto;
+   display: flex;
+   flex-direction: row;
+   padding: 5px;
+   width: 50%;
+   position: absolute;
+   z-index: 2;
+   top: 65px;
+`
+const Img = styled.img`
+   width: 250px;
+   height: 350px;
+   border: solid 2px red;
+   border-radius: 25px;
+`
+const Article = styled.article`
    ${mixins.column};
+   justify-content: center;
    color: ${colors.bg_tertiary};
    & h2 {
+      display: flex;
+      align-self: center;
       font-family: ${fontFamily.ff_secondary};
       font-size: ${fontSize.title};
-      margin-bottom: 15px;
+      margin-bottom: 70px;
    }
    & p {
       font-size: ${fontSize.subtitle};
+      margin-left: 35px;
       & span {
-         text-decoration: bold;
+         font-weigth: 600;
       }
    }
 `
@@ -38,18 +59,18 @@ const Section = styled.section`
 export default function Home() {
    return (
       <Main>
-         <Shape>
-            <Img>photo</Img>
-            <Section>
+         <Section>
+            <Img src={Moi} />
+            <Article>
                <h2>Bienvenue sur mon portfolio</h2>
                <p>
                   Je suis Audrey, <span>Intégratrice Web FrontEnd</span> et diplômée d'OpenClassrooms.
                   <br />
                   Ayant toujours été attirée par la <span>création</span> et les <span>technologies informatiques</span>, j'ai fusionné ces deux intérêts pour mettre mes compétences à votre service.
                </p>
-            </Section>
-            fond forme couleur
-         </Shape>
+            </Article>
+         </Section>
+         <Shape />
       </Main>
    )
 }
