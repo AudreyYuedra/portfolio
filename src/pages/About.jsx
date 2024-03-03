341d9b6af7c1d757a4e644063abd115557e05c6f
Audrey Luquet <audrey.luquet@tutanota.com>
2024-03-03 16:41:47 +0100

    modif mise en page About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,93 +5,100 @@ import styled from "styled-components"
 import { mixins } from "../styles/mixins"
 import { colors, fontFamily, fontSize } from "../styles/variables"
 const Main = styled.main`
-   width: 100%;
-   padding: 20px;
+   margin: 50px 0;
 `
 const Title = styled.h2`
-   width: 100% ${mixins.rowCenter};
+   display: flex;
+   justify-content: center;
    font-family: ${fontFamily.ff_secondary};
    font-size: ${fontSize.title};
    color: ${colors.bg_secondary};
-   margin-bottom: 20px;
+   margin-bottom: 35px;
+`
+const ContainerCol = styled.div`
+   ${mixins.rowSpaceBetween};
 `
 const Perso = styled.section`
+   width: 48%;
    ${mixins.column};
-   & article {
-      padding: 10px;
-      margin-bottom: 15px;
-      & h3 {
-         font-size: ${fontSize.subtitle};
-         text-decoration: bold;
-         margin-bottom: 5px;
-      }
-      & p {
-         margin-bottom: 10px;
-      }
-      & span {
-         font-size: ${fontSize.txt_about};
-      }
+   padding: 20px;
+`
+const Prez = styled.article`
+   margin-bottom: 60px;
+   & p {
+      margin-bottom: 5px;
+   }
+   & span {
+      margin-top: 5px;
+      font-style: italic;
    }
 `
-const Prez = styled.p`
-   padding: 10px;
-   margin-bottom: 15px;
+const Scolaire = styled.article`
+   & p {
+      margin-bottom: 10px;
+   }
    & span {
-      txt-decoration: italic;
+      font-size: ${fontSize.txt_about};
    }
 `
 const Pro = styled.section`
+   width: 48%;
    ${mixins.column};
-   & h3 {
-      font-size: ${fontSize.subtitle};
-      text-decoration: bold;
-      margin-bottom: 5px;
-   }
+   padding: 20px;
+`
+const H3 = styled.h3`
+   font-size: ${fontSize.subtitle};
+   font-weigth: 700;
+   margin-bottom: 10px;
+   display: flex;
+   justify-content: center;
 `
 
 export default function About() {
    return (
       <Main>
          <Title>À Propos</Title>
+         <ContainerCol>
+            <Perso>
+               <Prez>
+                  <p>Je suis située en région parisienne, plus précisément en Seine-et-marne.</p>
+                  <p>
+                     Passionnée par l'écriture depuis de nombreuses années, j'ai décidée d'utiliser mes compétences en création pour vous aider à construire vote site web personnel ou professionnel,
+                     qui est le reflet de votre histoire et de votre personnalité.
+                  </p>
+                  <p>
+                     Le travail en équipe est une notion importante pour moi, les challenges ne me font pas peur, et je suis toujours prête à apprendre de nouvelles technologies afin de répondre au
+                     mieux à vos demandes.
+                  </p>
+                  <span>Je suis disponible immédiatement et motivée à collaborer avec vous !</span>
+               </Prez>
+               <Scolaire>
+                  <H3>Scolaire</H3>
+                  <p>
+                     Formation Intégrateur Web
+                     <br />
+                     <span>OpenClassrooms</span>
+                  </p>
+                  <p>
+                     Niveau 1ère année LLCER Japonais
+                     <br />
+                     <span>Inalco</span>
+                  </p>
+                  <p>
+                     DAEU A - option Littéraire
+                     <br />
+                     <span>Paris Diderot</span>
+                  </p>
+               </Scolaire>
+            </Perso>
 
-         <Perso>
-            <Prez>
-               Je suis située en région parisienne, plus précisément en Seine-et-marne.
-               <br />
-               Passionnée par l'écriture depuis de nombreuses années, j'ai décidée d'utiliser mes compétences en création pour vous aider à construire vote site web personnel ou professionnel, qui est
-               le reflet de votre histoire et de votre personnalité.
-               <br />
-               Le travail en équipe est une notion importante pour moi, les challenges ne me font pas peur, et je suis toujours prête à apprendre de nouvelles technologies afin de répondre au mieux à
-               vos demandes.
-               <br />
-               <span>Je suis disponible immédiatement et motivée à collaborer avec vous !</span>
-            </Prez>
-            <article>
-               <h3>Scolaire</h3>
-               <p>
-                  Formation Intégrateur Web
-                  <br />
-                  <span>OpenClassrooms</span>
-               </p>
-               <p>
-                  Niveau 1ère année LLCER Japonais
-                  <br />
-                  <span>Inalco</span>
-               </p>
-               <p>
-                  DAEU A - option Littéraire
-                  <br />
-                  <span>Paris Diderot</span>
-               </p>
-            </article>
-         </Perso>
-
-         <Pro>
-            <h3>Expéricence</h3>
-            {ProjectData.map((data) => (
-               <Exp cover={data.cover} title={data.title} />
-            ))}
-         </Pro>
+            <Pro>
+               <H3>Expéricence</H3>
+               {ProjectData.map((data) => (
+                  <Exp content={data.resume} title={data.title} />
+               ))}
+            </Pro>
+         </ContainerCol>
       </Main>
    )
 }
