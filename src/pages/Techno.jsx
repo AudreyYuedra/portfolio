4281874b6dd51ae0c754188f4b0ddfeb4a5d5fc7
Audrey Luquet <audrey.luquet@tutanota.com>
2024-03-06 15:12:09 +0100

    modif style techno (toujours prob img + key)

--- src/pages/Techno.jsx
+++ src/pages/Techno.jsx
@@ -4,64 +4,61 @@ import styled from "styled-components"
 import { mixins } from "../styles/mixins"
 import { colors, fontFamily, fontSize } from "../styles/variables"
 const Main = styled.main`
-   display: flex;
-   flex-direction: row;
-   padding: 30px 60px;
+   margin: 70px 0;
    ${mixins.column};
 `
 const ContainCol = styled.section`
    ${mixins.column};
-   margin-bottom: 70px;
+   margin-bottom: 50px;
+   & h2 {
+      ${mixins.titleH2};
+   }
    & p {
       display: flex;
       justify-content: center;
    }
 `
-const Title = styled.h2`
-   display: flex;
-   justify-content: center;
-   font-family: ${fontFamily.ff_secondary};
-   font-size: ${fontSize.title};
-   color: ${colors.bg_secondary};
-   margin-bottom: 30px;
-`
 const ContainRow = styled.section`
    ${mixins.rowSpaceBetween};
-   margin: 0 25px 30px 25px;
+   margin: 0 120px;
 `
 const SectionTech = styled.article`
    ${mixins.column};
    display: flex;
    align-items: center;
+   & h3 {
+      font-family: ${fontFamily.Satisfy};
+      color: ${colors.pastel_blue};
+      font-weight: 700;
+      margin-bottom: 15px;
+   }
    & ul {
       ${mixins.column};
       list-style-type: none;
       & li {
-         margin-bottom: 10px;
          display: flex;
          flex-direction: row;
+         margin-bottom: 10px;
+         border: solid 2px pink;
          & img {
             width: 50px;
-            heigth: 50px;
+            height: 50px;
+            border: solid 2px yellow;
          }
          & p {
-            dispaly: flex;
+            display: flex;
             align-self: center;
             padding-left: 10px;
          }
       }
    }
 `
-const H3 = styled.h3`
-   font-family: ${fontFamily.ff_secondary};
-   margin-bottom: 15px;
-`
 
 export default function Techno() {
    return (
       <Main>
          <ContainCol>
-            <Title>Technologies</Title>
+            <h2>Technologies</h2>
             <p>
                Voici les langages, frameworks et outils que j'utilise pour réaliser mes projets.
                <br />
@@ -72,12 +69,12 @@ export default function Techno() {
             {/* Sections par catégories */}
             {TechnoData.map((index) => (
                <SectionTech>
-                  <H3 key={index.id}>{index.category}</H3>
+                  <h3 key={index.id}>{index.category}</h3>
                   {/*liste techno */}
                   <ul>
                      {index.object.map((data) => (
                         <li key={data.id}>
-                           <img src={data.logo} key={data.id} />
+                           <img src={data.logo} alt={`logo ${data.name}`} />
                            <p>{data.name}</p>
                         </li>
                      ))}
