72549df8108e8002c551fcf6a17b82444484b403
Audrey Luquet <audrey.luquet@tutanota.com>
2024-03-06 15:28:39 +0100

    modif style Error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,26 @@
+import styled from "styled-components"
+import { mixins } from "../styles/mixins"
+import { colors, fontFamily, fontSize } from "../styles/variables"
+const Main = styled.main`
+   margin: 70px 0;
+   ${mixins.column};
+   ${mixins.totalCenter};
+   & h2 {
+      ${mixins.titleH2};
+   }
+   span {
+      color: ${colors.pastel_blue};
+      font-weight: 700;
+      margin-bottom: 20px;
+   }
+`
+
 export default function Error() {
    return (
-      <main>
-         <p>ERROR !!!</p>
-      </main>
+      <Main>
+         <h2>OUPS !</h2>
+         <span>Il semble y avoir eu une erreur.</span>
+         <p>La page que vous recherchez n'existe pas !</p>
+      </Main>
    )
 }
