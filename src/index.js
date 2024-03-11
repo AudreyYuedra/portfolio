acb0bc07683ef38d80911a7e81b08f3cc92c4269
Audrey Luquet <audrey.luquet@tutanota.com>
2024-03-11 08:38:44 +0100

    suppr build

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ root.render(
          <Route path="/Projects" element={<Projects />} />
          <Route path="/Techno" element={<Techno />} />
          <Route path="/Contact" element={<Contact />} />
+         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
    </Router>
