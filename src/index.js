3a95d7181a14998bc630b941918ce5ecd18bc91f
Audrey Luquet <audrey.luquet@tutanota.com>
2024-03-11 08:31:18 +0100

    suppr page error

--- src/index.js
+++ src/index.js
@@ -22,7 +22,6 @@ root.render(
          <Route path="/Projects" element={<Projects />} />
          <Route path="/Techno" element={<Techno />} />
          <Route path="/Contact" element={<Contact />} />
-         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
    </Router>
